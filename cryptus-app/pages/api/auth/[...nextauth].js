3e7ace1c6b6745eeada72d7306eac00cba1411a5
Duval <jeremie.duval@usherbrooke.ca>
2021-07-07 15:04:01 -0400

    Début de code pour le back end

--- cryptus-app/pages/api/auth/[...nextauth].js
+++ cryptus-app/pages/api/auth/[...nextauth].js
@@ -1,5 +1,9 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 const options = {
   // Configure one or more authentication providers
@@ -8,8 +12,39 @@ const options = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET
     }),
-    // ...add more providers here
+
+    Providers.Credentials({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'Credentials',
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: {  label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+  
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null or false then the credentials will be rejected
+          return null
+          // You can also Reject this callback with an Error or with a URL:
+          // throw new Error('error message') // Redirect to error page
+          // throw '/path/to/redirect'        // Redirect to a URL
+        }
+      }
+    })
+
+        // ...add more providers here
   ],
+
+  adapter: PrismaAdapter(prisma),
+
   debug: process.env.NODE_ENV === "development",
   secret: process.env.AUTH_SECRET,
   jwt: {
