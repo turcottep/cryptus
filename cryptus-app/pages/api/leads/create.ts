5b54fafefd25bd493c5ab760405eaff2266f9160
turcottep <philippe0turcotte@gmail.com>
2021-07-09 16:39:43 -0400

    added default data to users/create.ts

--- cryptus-app/pages/api/leads/create.ts
+++ cryptus-app/pages/api/leads/create.ts
@@ -6,11 +6,24 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     try {
         await prisma.user.create({
             data: {
-                username: 'Alice',
-                email: req.body.email,
-                description: 'yoooo',
-                views: 5,
-                likes: 2
+                username: "laflow3r",
+
+                email: "alexandre.lafleur@usherbrooke.ca",
+
+                description: "Je vais a MTL pour chercher un bike",
+
+                views: 6969,
+
+                likes: 420,
+
+                hash: "4d6da51cf568dd8037650a5ae0fe1c4344cca65361f3096d4b6a926614ab59bc",
+
+                wallets: {
+                    create: {
+                        external_url:
+                            "https://api.opensea.io/api/v1/assets?owner=0x0da2f3401296427d302326cdf208b79f83abc995&order_direction=asc&offset=0&limit=50",
+                    },
+                },
 
             },
         })
