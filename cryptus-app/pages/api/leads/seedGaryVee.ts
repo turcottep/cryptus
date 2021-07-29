cf0582736d1930ae87ea578cf129d754e36fa9d8
turcottep <philippe0turcotte@gmail.com>
2021-07-29 09:00:04 -0400

    added garyvee mock account

--- cryptus-app/pages/api/leads/seedGaryVee.ts
+++ cryptus-app/pages/api/leads/seedGaryVee.ts
@@ -7,18 +7,18 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
   try {
     await prisma.user.create({
       data: {
-        username: "laflow3r",
-        email: "alexandre.lafleur@usherbrooke.ca",
-        displayName: "Lafleur",
-        description: "Je vais a MTL pour chercher un bike",
-        views: 6000,
-        likes: 400,
+        username: "garyvee",
+        email: "garyvee@hotmail.com",
+        displayName: "Gary Vaynerchuk",
+        description: "Family First. CEO of @Vaynermedia. 🐈‍⬛ Creator of @veefriends. Investor in Twitter, Coinbase & more. Die hard @NYJets fan. @winetexts@vaynersports",
+        views: 555,
+        likes: 5,
         hash: "4d6da51cf568dd8037650a5ae0fe1c4344cca65361f3096d4b6a926614ab59bc",
         wallets: {
           create: {
-            address: "0x0da2f3401296427d302326cdf208b79f83abc995",
+            address: "0x5ea9681c3ab9b5739810f8b91ae65ec47de62119",
             external_url:
-              "https://api.opensea.io/api/v1/assets?owner=0x0da2f3401296427d302326cdf208b79f83abc995&order_direction=asc&offset=0&limit=50",
+              "https://api.opensea.io/api/v1/assets?owner=0x5ea9681c3ab9b5739810f8b91ae65ec47de62119&order_direction=asc&offset=0&limit=50",
           },
         },
       },
