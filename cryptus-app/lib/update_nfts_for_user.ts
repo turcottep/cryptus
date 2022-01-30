c7f5dec52dd3a0e066e92670cdd4b04ba442643b
turcottep <32850285+turcottep@users.noreply.github.com>
2022-01-29 23:44:06 -0500

    mirrored all relevant data for nft showcasing to our database

---
+++ cryptus-app/lib/update_nfts_for_user.ts
@@ -0,0 +1,58 @@
+
+export default async function updateNftsForUser(username: string, address: string, userId: string, absolute = true) {
+
+    let res;
+    let nfts_raw;
+
+    try {
+        var data;
+        //fetch nfts from opensea
+        res = await fetch("https://api.opensea.io/api/v1/assets?owner=" + address + "&order_direction=asc&offset=0&limit=50", {
+            headers: {
+                "Accept": "application/json",
+                "X-API-KEY": process.env.OPENSEA_API_KEY,
+            },
+        });
+        // res = await fetch(wallet.external_url);
+        data = await res.json();
+
+        nfts_raw = data.assets;
+    } catch (err) {
+        console.error(err);
+        console.log("response = ", res);
+        return null;
+    }
+
+    const nft_clean = nfts_raw.map(nft => {
+        return {
+            name: nft.name ?? nft.collection.name + " #" + nft.id,
+            image_url: nft.image_url,
+            description: nft.description,
+            collection: nft.collection.name,
+            collectionId: nft.id,
+            external_url: nft.permalink,
+        }
+    }
+    );
+
+    const base_url = absolute ? process.env.BASE_URL : '/'
+    try {
+        const res = await fetch(base_url + "api/nfts/update", {
+            method: "POST",
+            body: JSON.stringify({ nfts: nft_clean, username: username, userId: userId }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (res.status !== 200 && res.status !== 201) {
+            throw new Error("Error updating nfts for user");
+        }
+
+    } catch (e) {
+        console.error("Erreur :", e);
+        return null;
+    }
+
+    return nft_clean
+
+}
--- cryptus-app/pages/[userId]/index.tsx
+++ cryptus-app/pages/[userId]/index.tsx
@@ -5,52 +5,65 @@ import Mosaic from "../../components/viewer/Mosaic";
 import NavbarProfile from "../../components/navbars/navbar_profile/navbar_profile";
 import Profile from "../../components/profile/profile";
 import getUserByUsername from "../../lib/getUserByUsername";
+import update_nfts_for_user from "../../lib/update_nfts_for_user";
+import get_nfts_for_user from "../../lib/get_nfts_for_user";
 
 export default function post(props) {
   const router = useRouter();
-  const { username } = router.query;
+  const { userId } = router.query;
+  const username = userId
 
   return (
     <div className="bg-instagram">
       <main className="sm:max-w-lg mx-auto">
         <div className="flex flex-col items-center">
           <NavbarProfile name={username} />
-          <div className="mt-24 w-full">
-            <Profile {...props} />
-          </div>
-          <Mosaic {...props} />
+          {props.user ? (
+            <div className="mt-24 w-full">
+              <div className="">
+                <Profile {...props} />
+              </div>
+              <Mosaic {...props} />
+            </div>
+          ) : (
+            <div className="flex flex-col items-center justified-center text-black ">
+              User Not Found
+            </div>
+          )}
         </div>
       </main>
     </div>
   );
 }
 
+
 export async function getServerSideProps(context) {
   const username = context.query.userId;
   const user = await getUserByUsername(username, true);
+  if (!user) {
+    return {
+      props: {
+        assets: [],
+        user: null,
+      },
+    };
+  }
   let res;
   try {
-    var data;
-    for (const wallet of user.wallets) {
-      const address = wallet.address;
-      //fetch nfts from opensea
-      res = await fetch("https://api.opensea.io/api/v1/assets?owner=" + address + "&order_direction=asc&offset=0&limit=50", {
-        headers: {
-          "Accept": "application/json",
-          "X-API-KEY": process.env.OPENSEA_API_KEY,
-        },
-      });
-      // res = await fetch(wallet.external_url);
-      data = await res.json();
+    let nfts = await update_nfts_for_user(username, user.wallets[0].address, user.userId);
+    if (!nfts) {
+      nfts =
+        await get_nfts_for_user(username);
     }
-    if (!data) {
-      return {
-        props: { assets: [], user: user },
-      };
-    }
+    nfts.sort((a, b) => {
+      return b.collection - a.collection;
+    });
+    // console.log("nfts===", nfts);
     return {
-      props: { assets: data.assets, user: user },
+      props: { assets: nfts, user },
     };
+
+
   } catch (err) {
     console.error(err);
     console.error(err);
