340d3f9b294fc0162dc31c8c497ca787730661c4
Duval <jeremie.duval@usherbrooke.ca>
2022-02-10 16:31:29 -0500

    Get collection's token from collection's contract address added in lib

---
+++ cryptus-app/lib/get_collection_token.ts
@@ -0,0 +1,16 @@
+export default async function getCollectionToken(contractaddress: string) {
+  const url =
+    "https://api.etherscan.io/api" +
+    "?module=stats" +
+    "&action=tokensupply" +
+    "&contractaddress=" +
+    contractaddress +
+    "&apikey=7CKI7M6YS4TBYYNK47VK9KTJDFV1254BV4";
+
+  const response = await fetch(url, { method: "POST", body: "a=1" });
+  const data = await response.json();
+
+  console.log(data.result);
+
+  return data.result;
+}
