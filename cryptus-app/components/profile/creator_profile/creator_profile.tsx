7766f8d5fa9130d7b3ddb3a175a03404f09f03b3
Alexandre Lafleur <alexandre.lafleur@usherbrooke.ca>
2022-02-23 15:35:42 -0500

    profile_wallet_viewer fixed

--- cryptus-app/components/profile/creator_profile/creator_profile.tsx
+++ cryptus-app/components/profile/creator_profile/creator_profile.tsx
@@ -7,7 +7,6 @@ import CreatorHeader from "./creator_header/creator_header";
 import { profile_props } from "../../../lib/data_types";
 import ProfileWalletViewer from "../../wallet_viewer/profile_wallet_viewer/profile_wallet_viewer";
 import Footer from "../../footer/footer";
-//import Footer from "../../footer/footer"
 
 export default function CreatorProfile(props: profile_props) {
   const mock_props = get_mock_props() as profile_props;
