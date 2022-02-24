6d204ecc1ce1e5db60edcae5bb1214895fd2dfac
turcottep <32850285+turcottep@users.noreply.github.com>
2022-02-24 17:18:00 -0500

    trying to fox the beat

--- cryptus-app/next.config.js
+++ cryptus-app/next.config.js
@@ -8,4 +8,8 @@ module.exports = withPWA({
     locales: ["en"],
     defaultLocale: "en",
   },
+  // webpack: (config, { isServer }) => {
+  //   if (!isServer) config.resolve.fallback.fs = false;
+  //   return config;
+  // },
 });
