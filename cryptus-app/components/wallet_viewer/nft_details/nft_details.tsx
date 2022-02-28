4d28dc703b6ccf38d1a25f9ad3e3e0b2469e9c9a
Duval <jeremie.duval@usherbrooke.ca>
2022-02-28 17:32:59 -0500

    Properties and listed price added

--- cryptus-app/components/wallet_viewer/nft_details/nft_details.tsx
+++ cryptus-app/components/wallet_viewer/nft_details/nft_details.tsx
@@ -1,5 +1,5 @@
 //react and css
-import React from "react";
+import React, { useState, useEffect } from "react";
 import s from "./nft_details.module.scss";
 
 import { nft } from "../../../lib/data_types";
@@ -9,6 +9,7 @@ import NftInfo from "./nft_infos/nft_info";
 import NftPicture from "./nft_picture/nft_picture";
 import NFTProperties from "./nft_properties/nft_properties";
 import NFTRankInCollection from "./nft_rank_in_collection/nft_rank_in_collection";
+import GetCollectionTokens from "../../../lib/get_collection_token";
 
 type nft_details_props = {
   nft: nft;
@@ -20,14 +21,48 @@ type nft_details_props = {
 
 export default function NFTDetails(props: nft_details_props) {
   const { nft, rank } = props;
+  let properties = nft.properties;
+
+  let [collection_size, setCollection_size] = useState(Number);
+  let [rarity_rank, setraRity_rank] = useState(Number);
+
+  useEffect(() => {
+    const collectionCall = async () => {
+      try {
+        const collectionSize: number = await GetCollectionTokens(
+          nft.collection_address
+        );
+        setCollection_size(collectionSize);
+        // Rarity rank is calculated from it's traits and rounded the result. The equation is :sum(1/(nb_with_trait/total_count))
+        const sorted_traits = nft.properties.sort((a, b) => {
+          return a.count - b.count;
+        });
+        const rarity = Math.round(
+          sorted_traits
+            .map((trait) => {
+              trait.rarity = trait.count / collectionSize;
+              return 1 / trait.rarity;
+            })
+            .reduce((partialSum, a) => partialSum + a, 0)
+        );
+        setraRity_rank(rarity);
+
+        console.log("rarity : ", rarity);
+        console.log("collection_size : ", collectionSize);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    collectionCall();
+  }, []);
 
   return (
     <div className={s.container}>
       <NftHeader />
       <NftPicture image_url={nft.image_url} description={nft.description} />
       <NftInfo {...props} />
-      <NFTRankInCollection position={rank.position} total={rank.total} />
-      <NFTProperties properties={nft.properties} />
+      <NFTRankInCollection position={rarity_rank} total={collection_size} />
+      <NFTProperties properties={properties} />
     </div>
   );
 }
--- cryptus-app/components/wallet_viewer/nft_details/nft_infos/nft_info.tsx
+++ cryptus-app/components/wallet_viewer/nft_details/nft_infos/nft_info.tsx
@@ -1,6 +1,7 @@
 //react and css
 import React, { useState, useEffect } from "react";
 import s from "./nft_info.module.scss";
+import getNFTListedPrice from "../../../../lib/get_nft_listed_price";
 
 //external exports
 
@@ -11,6 +12,26 @@ import NftBuyButton from "./nft_buy_button/nft_buy_button";
 export default function NftInfo(props: { nft: nft }) {
   const { name, last_sale_price, external_url, collection } = props.nft;
   const collection_stripped = collection.replace(/\s/g, ""); // remove all spaces from collection name
+
+  let [listed_price, setListed_price] = useState(0);
+
+  useEffect(() => {
+    collectionCall();
+  }, []);
+
+  const collectionCall = async () => {
+    try {
+      setListed_price(
+        await getNFTListedPrice(
+          props.nft.collection_address,
+          props.nft.token_id
+        )
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className={s.container}>
       <div className={s.left}>
@@ -19,7 +40,7 @@ export default function NftInfo(props: { nft: nft }) {
         </a>
         <div className={s.name}>{name}</div>
       </div>
-      <NftBuyButton price={last_sale_price} url={external_url} />
+      <NftBuyButton price={listed_price} url={external_url} />
     </div>
   );
 }
