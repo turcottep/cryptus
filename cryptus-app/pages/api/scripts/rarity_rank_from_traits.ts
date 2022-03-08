885dc099c715ccb4df27e9ef05796a6ed5ffc1c0
Duval <jeremie.duval@usherbrooke.ca>
2022-03-08 16:19:40 -0500

    Collection with the wrong attributes break and return null

--- cryptus-app/pages/api/scripts/rarity_rank_from_traits.ts
+++ cryptus-app/pages/api/scripts/rarity_rank_from_traits.ts
@@ -1,12 +1,36 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import main from "../../../scripts/get_rarity_rank_of_nft";
+import turcotte from "../../../public/turcotte";
+import GetNameWithoutSpaces from "../../../lib/get_name_without_spaces";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   try {
-    await main(
-      "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d",
-      "BoredApeYatchClub"
-    );
+    // for (const collection of turcotte.collections) {
+    //   console.log("Collection names : ", collection.name);
+    //   try {
+    //     await main(
+    //       collection.nfts[0].collection_address,
+    //       GetNameWithoutSpaces(collection.name)
+    //     );
+    //     await new Promise((resolve) => setTimeout(resolve, 10000));
+    //   } catch (error) {
+    //     console.log("An error occured...");
+    //   }
+    // }
+
+    // await main("0x06012c8cf97bead5deae237070f9587f8e7a266d", "CryptoKitties");
+    // await main(
+    //   "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d",
+    //   "BoredApeYatchClub"
+    // );
+    // await main("0x2f2d5aa0efdb9ca3c9bb789693d06bebea88792f", "BlockCities");
+    await main("0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB", "CryptoPunks");
+    // await main("0x43b557b1f923f1cb4ecd05ff3beac7b933bedb46", "JustinRoiland");
+    // await main("0x73da73ef3a6982109c4d5bdb0db9dd3e3783f313", "MyCurioCards");
+    // await main("0x495f947276749ce646f68ac8c248420045cb7b5e", "SlayerX");
+    // await main("0x6fc355d4e0ee44b292e50878f49798ff755a5bbc", "DeadHeads");
+    // await main("0x78f28143902e9346526933e3c2eda2662d1cd1f7", "DeadTickets");
+    // await main("0x7645eec8bb51862a5aa855c40971b2877dae81af", "LONDONGIFTbyPOB");
     const data = { deez: "yo" };
 
     res.json(data);
--- cryptus-app/scripts/get_rarity_rank_of_nft.ts
+++ cryptus-app/scripts/get_rarity_rank_of_nft.ts
@@ -13,125 +13,139 @@ async function generateRarity(
   const totalNum = nfts.total;
   const pageSize = nfts.page_size;
   let allNfts = nfts.result;
+  // console.log("total num : ", totalNum);
+  // console.log("Page size : ", pageSize);
+  // console.log("First nft : ", allNfts[0]);
+  // console.log("First nft metadata: ", JSON.parse(allNfts[0].metadata));
 
-  for (let i = pageSize; i < totalNum; i += pageSize) {
-    console.log("i : ", i);
-    const nfts = (await Moralis.Web3API.token.getAllTokenIds({
-      address: collectionAddress,
-      offset: i,
-    })) as any;
-    allNfts.push(...nfts.result);
-    await new Promise((resolve) => setTimeout(resolve, 2500));
-  }
-
-  let metadata = allNfts.map((e) => JSON.parse(e.metadata).attributes);
-  // console.log(metadata[0]);
-
-  // console.log("Num of nfts : ", allNfts.length);
-
-  let tally = { TraitCount: {} };
-
-  for (let j = 0; j < metadata.length; j++) {
-    let nftTraits = metadata[j].map((e) => e.trait_type);
-    let nftValues = metadata[j].map((e) => e.value);
-
-    let numOfTraits = nftTraits.length;
-
-    if (tally.TraitCount[numOfTraits]) {
-      tally.TraitCount[numOfTraits]++;
-    } else {
-      tally.TraitCount[numOfTraits] = 1;
+  if (
+    JSON.parse(allNfts[0].metadata).attributes.trait_type &&
+    JSON.parse(allNfts[0].metadata).attributes.value
+  ) {
+    for (let i = pageSize; i < totalNum; i += pageSize) {
+      console.log("i : ", i);
+      const nfts = (await Moralis.Web3API.token.getAllTokenIds({
+        address: collectionAddress,
+        offset: i,
+      })) as any;
+      allNfts.push(...nfts.result);
+      await new Promise((resolve) => setTimeout(resolve, 5000));
     }
 
-    for (let i = 0; i < nftTraits.length; i++) {
-      let current = nftTraits[i];
-      if (tally[current]) {
-        tally[current].occurences++;
+    let metadata = allNfts.map((e) => JSON.parse(e.metadata).attributes);
+    // console.log(metadata[0]);
+
+    // console.log("Num of nfts : ", allNfts.length);
+
+    let tally = { TraitCount: {} };
+
+    for (let j = 0; j < metadata.length; j++) {
+      let nftTraits = metadata[j].map((e) => e.trait_type);
+      let nftValues = metadata[j].map((e) => e.value);
+
+      let numOfTraits = nftTraits.length;
+
+      if (tally.TraitCount[numOfTraits]) {
+        tally.TraitCount[numOfTraits]++;
       } else {
-        tally[current] = { occurences: 1 };
+        tally.TraitCount[numOfTraits] = 1;
       }
 
-      let currentValue = nftValues[i];
-      if (tally[current][currentValue]) {
-        tally[current][currentValue]++;
-      } else {
-        tally[current][currentValue] = 1;
+      for (let i = 0; i < nftTraits.length; i++) {
+        let current = nftTraits[i];
+        if (tally[current]) {
+          tally[current].occurences++;
+        } else {
+          tally[current] = { occurences: 1 };
+        }
+
+        let currentValue = nftValues[i];
+        if (tally[current][currentValue]) {
+          tally[current][currentValue]++;
+        } else {
+          tally[current][currentValue] = 1;
+        }
       }
     }
-  }
-  const collectionAttributes = Object.keys(tally);
-  let nftArr = [];
-  let nftDictByTokenId = {};
+    const collectionAttributes = Object.keys(tally);
+    let nftArr = [];
+    let nftDictByTokenId = {};
 
-  for (let j = 0; j < metadata.length; j++) {
-    let current = metadata[j];
-    let totalRarity = 0;
+    for (let j = 0; j < metadata.length; j++) {
+      let current = metadata[j];
+      let totalRarity = 0;
 
-    // Present traits
-    for (let i = 0; i < current.length; i++) {
-      let rarityScore =
-        1 / (tally[current[i].trait_type][current[i].value] / totalNum);
-      current[i].rarityScore = rarityScore;
-      totalRarity += rarityScore;
-    }
+      // Present traits
+      for (let i = 0; i < current.length; i++) {
+        let rarityScore =
+          1 / (tally[current[i].trait_type][current[i].value] / totalNum);
+        current[i].rarityScore = rarityScore;
+        totalRarity += rarityScore;
+      }
 
-    // Num of traits
-    let rarityScoreNumTraits =
-      1 / (tally.TraitCount[Object.keys(current).length] / totalNum);
-    current.push({
-      trait_type: "TraitCount",
-      value: Object.keys(current).length,
-      rarityScore: rarityScoreNumTraits,
-    });
-    totalRarity += rarityScoreNumTraits;
+      // Num of traits
+      let rarityScoreNumTraits =
+        1 / (tally.TraitCount[Object.keys(current).length] / totalNum);
+      current.push({
+        trait_type: "TraitCount",
+        value: Object.keys(current).length,
+        rarityScore: rarityScoreNumTraits,
+      });
+      totalRarity += rarityScoreNumTraits;
 
-    // Not present traits
-    if (current.length < collectionAttributes.length) {
-      let nftAttributes = current.map((e) => e.trait_type);
-      let absent = collectionAttributes.filter(
-        (e) => !nftAttributes.includes(e)
-      );
+      // Not present traits
+      if (current.length < collectionAttributes.length) {
+        let nftAttributes = current.map((e) => e.trait_type);
+        let absent = collectionAttributes.filter(
+          (e) => !nftAttributes.includes(e)
+        );
 
-      absent.forEach((type) => {
-        let rarityScoreNull =
-          1 / ((totalNum - tally[type].occurences) / totalNum);
-        current.push({
-          trait_type: type,
-          value: null,
-          rarityScore: rarityScoreNull,
+        absent.forEach((type) => {
+          let rarityScoreNull =
+            1 / ((totalNum - tally[type].occurences) / totalNum);
+          current.push({
+            trait_type: type,
+            value: null,
+            rarityScore: rarityScoreNull,
+          });
+          totalRarity += rarityScoreNull;
         });
-        totalRarity += rarityScoreNull;
+      }
+
+      nftArr.push({
+        Attributes: JSON.stringify(current),
+        Rarity: totalRarity,
+        token_id: allNfts[j].token_id,
+        collection_size: totalNum,
       });
     }
 
-    nftArr.push({
-      Attributes: JSON.stringify(current),
-      Rarity: totalRarity,
-      token_id: allNfts[j].token_id,
-      collection_size: totalNum,
+    nftArr.sort((a, b) => b.Rarity - a.Rarity);
+
+    nftArr.forEach((nft, index) => {
+      nftDictByTokenId[nft.token_id] = {
+        Attributes: nft.Attributes,
+        Rarity: nft.Rarity,
+        Collection_size: nft.collection_size,
+        Rarity_rank: index,
+      };
     });
+
+    const nftsToSave = { [collectionAddress]: nftDictByTokenId };
+    // save to file
+    // fs.writeFileSync(
+    //   "./scripts/nft_rank/" + collectionName + ".json",
+    //   JSON.stringify(nftsToSave, null, 2)
+    // );
+    // console.log("metadata : ", metadata[0]);
+    // console.log("nftArr : ", nftArr[0]);
+    return nftsToSave;
+  } else {
+    console.log(
+      "The metadata of this collection is not written in the right way"
+    );
+    return null;
   }
-
-  nftArr.sort((a, b) => b.Rarity - a.Rarity);
-
-  nftArr.forEach((nft, index) => {
-    nftDictByTokenId[nft.token_id] = {
-      Attributes: nft.Attributes,
-      Rarity: nft.Rarity,
-      Collection_size: nft.collection_size,
-      Rarity_rank: index,
-    };
-  });
-
-  // save to file
-  const nftsToSave = { [collectionAddress]: nftDictByTokenId };
-  fs.writeFileSync(
-    "./scripts/nft_rank/" + collectionName + ".json",
-    JSON.stringify(nftsToSave, null, 2)
-  );
-
-  // console.log("metadata : ", metadata[0]);
-  console.log("nftArr : ", nftArr[0]);
 }
 
 async function main(collectionAddress: string, collectionName: string) {
@@ -143,7 +157,8 @@ async function main(collectionAddress: string, collectionName: string) {
 
   Moralis.start({ serverUrl, appId });
 
-  generateRarity(collectionAddress, collectionName);
+  const nftsWithRarity = generateRarity(collectionAddress, collectionName);
+  // return nftsWithRarity;
 }
 
 export default main;
