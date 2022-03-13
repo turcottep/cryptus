79406b850c98bfe070b040100a84921177af5c18
Guillaume <90265041+GuillaumeBrass@users.noreply.github.com>
2022-03-13 16:46:51 -0400

    prettier changes

--- cryptus-app/components/wallet_manager/wallet_list_display/wallet_list_display.tsx
+++ cryptus-app/components/wallet_manager/wallet_list_display/wallet_list_display.tsx
@@ -3,16 +3,7 @@ import React, { useState, useEffect } from "react";
 import s from "./wallet_list_display.module.scss";
 import { walletsType } from "../wallet_manager";
 
-
 export default function WalletListDisplay(props: { wallets: walletsType[] }) {
-  const wallets = props.wallets.map((wallet, i)=>(
-      <div>
-          {wallet.address}
-      </div>
-  ))
-  return (
-      <ul>
-          {wallets}
-      </ul>
-  )
+  const wallets = props.wallets.map((wallet, i) => <div>{wallet.address}</div>);
+  return <ul className={s.container}>{wallets}</ul>;
 }
