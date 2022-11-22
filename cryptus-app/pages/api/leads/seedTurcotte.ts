40e1aab0cb69a8017f965c774b5b551a211da1fb
turcottep <32850285+turcottep@users.noreply.github.com>
2022-11-22 14:07:27 -0500

    test with new guy

--- cryptus-app/pages/api/leads/seedTurcotte.ts
+++ cryptus-app/pages/api/leads/seedTurcotte.ts
@@ -3,22 +3,21 @@ import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-
   try {
     await prisma.user.create({
       data: {
-        username: "turcotte",
-        email: "turcotte@usherbrooke.ca",
-        displayName: "Turcotte",
-        description: "Je vais a MTL pour chercher un bike",
+        username: "sultanpink",
+        email: "sultanpink@gmail.com",
+        displayName: "sultanpink.eth",
+        description: "Guy with historical networth",
         views: 6000,
         likes: 400,
         hash: "4d6da51cf568dd8037650a5ae0fe1c4344cca65361f3096d4b6a926614ab59bc",
         wallets: {
           create: {
-            address: "0x0da2f3401296427d302326cdf208b79f83abc995",
+            address: "0x052288F424Ec1a127093E29BaDC21Dd2ddb860A1".toLowerCase(),
             external_url:
-              "https://api.opensea.io/api/v1/assets?owner=0x0da2f3401296427d302326cdf208b79f83abc995&order_direction=asc&offset=0&limit=2",
+              "https://api.opensea.io/api/v1/assets?owner=0x052288F424Ec1a127093E29BaDC21Dd2ddb860A1&order_direction=asc&offset=0&limit=2",
           },
         },
       },
@@ -27,12 +26,11 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     res.status(201);
 
     res.json({});
-
   } catch (e) {
     res.status(500);
 
     res.json({ error: "Unable to add lead" });
-    console.error("Unable to add lead", e)
+    console.error("Unable to add lead", e);
   } finally {
     await prisma.$disconnect();
   }
--- cryptus-app/pages/api/networth/historical_networth.ts
+++ cryptus-app/pages/api/networth/historical_networth.ts
@@ -3,116 +3,116 @@ import prisma from "../../../lib/prisma";
 import fs from "fs";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const address = "0x052288F424Ec1a127093E29BaDC21Dd2ddb860A1".toLowerCase();
+
   try {
-    // prisma.$connect();
+    prisma.$connect();
 
-    const address = "0xed2ab4948bA6A909a7751DEc4F34f303eB8c7236".toLowerCase();
+    const first_block = 10916166;
+    const ending_block = 16018856;
+    const action = "tokennfttx";
 
-    // const first_block = 10916166;
-    // const ending_block = 16018856;
-    // const action = "tokennfttx";
+    const txs = await get_transactions_from_etherscan(
+      address,
+      first_block,
+      ending_block,
+      action
+    );
 
-    // const txs = await get_transactions_from_etherscan(
-    //   address,
-    //   first_block,
-    //   ending_block,
-    //   action
-    // );
+    console.log(txs);
 
-    // console.log(txs);
+    // sort txs by timestamp
+    txs.sort((a, b) => {
+      return parseInt(a.timeStamp) - parseInt(b.timeStamp);
+    });
 
-    // // sort txs by timestamp
-    // txs.sort((a, b) => {
-    //   return parseInt(a.timeStamp) - parseInt(b.timeStamp);
-    // });
+    // get earliest timestamp
+    const first_day = txs[0].timeStamp;
+    const first_day_date = new Date(parseInt(first_day) * 1000);
+    const first_day_date_string = first_day_date.toISOString().split("T")[0];
+    const last_day = txs[txs.length - 1].timeStamp;
+    const last_day_date = new Date(parseInt(last_day) * 1000);
+    const last_day_date_string = last_day_date.toISOString().split("T")[0];
 
-    // // get earliest timestamp
-    // const first_day = txs[0].timeStamp;
-    // const first_day_date = new Date(parseInt(first_day) * 1000);
-    // const first_day_date_string = first_day_date.toISOString().split("T")[0];
-    // const last_day = txs[txs.length - 1].timeStamp;
-    // const last_day_date = new Date(parseInt(last_day) * 1000);
-    // const last_day_date_string = last_day_date.toISOString().split("T")[0];
+    console.log("first_day_date_string", first_day_date_string);
+    console.log("last_day_date_string", last_day_date_string);
 
-    // console.log("first_day_date_string", first_day_date_string);
-    // console.log("last_day_date_string", last_day_date_string);
+    // generate all days from first day to last day
+    const all_days = {};
+    const current_day = new Date(first_day_date_string);
+    const last_day_date_object = new Date(last_day_date_string);
+    while (current_day <= last_day_date_object) {
+      const day_object = {};
+      const day_string = current_day.toISOString().split("T")[0];
+      day_object["day"] = day_string;
+      all_days[day_string] = day_object;
+      current_day.setUTCDate(current_day.getUTCDate() + 1);
+    }
+    console.log("all_days", all_days);
 
-    // // generate all days from first day to last day
-    // const all_days = {};
-    // const current_day = new Date(first_day_date_string);
-    // const last_day_date_object = new Date(last_day_date_string);
-    // while (current_day <= last_day_date_object) {
-    //   const day_object = {};
-    //   const day_string = current_day.toISOString().split("T")[0];
-    //   day_object["day"] = day_string;
-    //   all_days[day_string] = day_object;
-    //   current_day.setUTCDate(current_day.getUTCDate() + 1);
-    // }
-    // console.log("all_days", all_days);
+    // list of all the collections that the user owns
+    const collections = [];
+    for (const tx of txs) {
+      collections.push(tx.contractAddress.toLowerCase());
+    }
 
-    // // list of all the collections that the user owns
-    // const collections = [];
-    // for (const tx of txs) {
-    //   collections.push(tx.contractAddress.toLowerCase());
-    // }
+    // add txs to all_days
+    for (const tx of txs) {
+      const tx_date = new Date(parseInt(tx.timeStamp) * 1000);
+      const tx_date_string = tx_date.toISOString().split("T")[0];
+      // console.log("tx_date_string", tx_date_string);
 
-    // // add txs to all_days
-    // for (const tx of txs) {
-    //   const tx_date = new Date(parseInt(tx.timeStamp) * 1000);
-    //   const tx_date_string = tx_date.toISOString().split("T")[0];
-    //   // console.log("tx_date_string", tx_date_string);
+      const tx_day = all_days[tx_date_string];
+      if (tx_day["txs"] == undefined) {
+        tx_day["txs"] = [];
+      }
+      tx_day["txs"].push(tx);
+    }
 
-    //   const tx_day = all_days[tx_date_string];
-    //   if (tx_day["txs"] == undefined) {
-    //     tx_day["txs"] = [];
-    //   }
-    //   tx_day["txs"].push(tx);
-    // }
+    console.log("all_days", all_days);
 
-    // console.log("all_days", all_days);
+    console.log("collections", collections);
 
-    // console.log("collections", collections);
+    console.log(
+      "inculdes",
+      collections.includes("0x60e4d786628fea6478f785a6d7e704777c86a7c6")
+    );
 
-    // console.log(
-    //   "inculdes",
-    //   collections.includes("0x60e4d786628fea6478f785a6d7e704777c86a7c6")
-    // );
+    console.log("getting collections from db");
 
-    // console.log("getting collections from db");
+    const existing_collections = {};
 
-    // const existing_collections = {};
+    for (const collection_address of collections) {
+      try {
+        const contract_address = collection_address; //"0x60e4d786628fea6478f785a6d7e704777c86a7c6";
+        console.log("contract_address", contract_address);
+        const address_cropped = contract_address.substring(1);
+        const viewing_mode = "month";
 
-    // for (const collection_address of collections) {
-    //   try {
-    //     const contract_address = collection_address; //"0x60e4d786628fea6478f785a6d7e704777c86a7c6";
-    //     console.log("contract_address", contract_address);
-    //     const address_cropped = contract_address.substring(1);
-    //     const viewing_mode = "month";
+        const query = `SELECT * FROM marketsales.${
+          address_cropped + "_" + viewing_mode
+        };`;
+        const answer = await prisma.$queryRaw(query);
+        console.log("answer", answer);
+        existing_collections[contract_address] = answer;
+      } catch (error) {
+        if (error.meta.message.includes("does not exist")) {
+          console.log("collection does not exist in db");
+        } else {
+          console.log(error);
+        }
+      }
+    }
 
-    //     const query = `SELECT * FROM marketsales.${
-    //       address_cropped + "_" + viewing_mode
-    //     };`;
-    //     const answer = await prisma.$queryRaw(query);
-    //     console.log("answer", answer);
-    //     existing_collections[contract_address] = answer;
-    //   } catch (error) {
-    //     if (error.meta.message.includes("does not exist")) {
-    //       console.log("collection does not exist in db");
-    //     } else {
-    //       console.log(error);
-    //     }
-    //   }
-    // }
+    console.log("existing_collections", existing_collections);
 
-    // console.log("existing_collections", existing_collections);
-
-    // // save to file
-    // console.log("saving to file");
-    // fs.writeFileSync(
-    //   "existing_collections.json",
-    //   JSON.stringify(existing_collections, null, 2)
-    // );
-    // fs.writeFileSync("all_days.json", JSON.stringify(all_days, null, 2));
+    // save to file
+    console.log("saving to file");
+    fs.writeFileSync(
+      "existing_collections.json",
+      JSON.stringify(existing_collections, null, 2)
+    );
+    fs.writeFileSync("all_days.json", JSON.stringify(all_days, null, 2));
 
     // read from file
     console.log("reading from file");
@@ -156,6 +156,7 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     }
 
     let i = 0;
+    const networth_history = [];
 
     for (const day of trading_days_list) {
       // console.log("day", day);
@@ -213,6 +214,7 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
         }
       }
       day["networth"] = networth;
+      networth_history.push(networth);
 
       i++;
     }
@@ -220,6 +222,28 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     // console.log("trading_days_list", trading_days_list);
 
     // let networth = 0;
+    console.log("networth_history", networth_history);
+
+    // save to user
+    const user_res = await prisma.wallet.findUnique({
+      where: {
+        address: address,
+      },
+      select: {
+        User: true,
+      },
+    });
+    console.log("user_res", user_res);
+
+    const res_update = await prisma.user.update({
+      where: {
+        id: user_res.User.id,
+      },
+      data: {
+        networth_history: networth_history,
+      },
+    });
+
     console.log("done");
 
     res.status(201);
